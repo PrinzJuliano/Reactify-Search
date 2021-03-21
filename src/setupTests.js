dfd6c48134005b6f30974e93f8b1cbf95c2460e1
PrinzJuliano <PrinzJuliano@users.noreply.github.com>
2021-03-21 16:34:12 +0100

    Changed tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,25 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+const mock = () => {
+  let storage = {};
+  return {
+    getItem: (key) => (key in storage ? storage[key] : null),
+    setItem: (key, value) => (storage[key] = value || ""),
+    removeItem: (key) => delete storage[key],
+    clear: () => (storage = {}),
+  };
+};
+
+Object.defineProperty(window, "localStorage", { value: mock() });
+Object.defineProperty(window, "sessionStorage", { value: mock() });
+Object.defineProperty(window, "getComputedStyle", {
+  value: () => ["-webkit-appearance"],
+});
+
+Object.defineProperty(window, "__env", {
+  value: { env: { backendUrl: "mocked URl" } },
+});
+
+window.localStorage.theme = "dark";
